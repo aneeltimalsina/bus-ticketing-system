4cd215bf1f3898b07b8b3859ce440302dc5fba0d
Anil Timalsina <aniltimalsina40@gmail.com>
2023-07-16 14:55:26 +0545

    Feature: make Layout

---
+++ includes/header.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="row">
+                <div class="col-3 text-center">
+                    <a href="/"> <img src="/assets/images/logo.png" height="100" alt="Online Bus Ticket System"></a>
+                </div>
+                <div class="col h1">
+                    <a href="" style="line-height: 100px; text-decoration: none; color: #00067a; text-align: left">
+                        Online Bus Ticket System
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- login.php
+++ login.php
@@ -4,13 +4,13 @@ ob_start()
 
 <div class="container">
     <div class="row">
-        <div class="col h1">This is Index Page</div>
+        <div class="col h1">This is login Portal Page</div>
     </div>
 </div>
 
 <?php
 $content = ob_get_contents();
 ob_end_clean();
-$title="Bus Sewa";
+$title = "Passenger Login Portal";
 include 'layouts/layout.php'
 ?>
